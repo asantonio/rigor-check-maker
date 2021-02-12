574536476127bcb16a10d08409cc43a14462c109
asantonio <alastair.antonio@gmail.com>
2021-02-11 22:33:47 -0800

    initial working version

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # rigor_check_maker
  script to create rigor real browser checks
+
+This script will leverates the Rigor API. To create a Real Browser Check, we are going to use: `POST /v2/checks/real_browsers`
+
+THe API can be played with online via: 
+https://monitoring-api.rigor.com/docs?url=%2Fv2%2Fdocs#!/Real_Browser_Checks/createRealBrowserCheck
+
+## Prerequisites
+You may need to install `jq` if you do not have it already installed in your shell. You can install via brew by running, for example `brew install jq`
+
+## How to use
+call create_check.sh to create a monitor or set of monitors (if you pass in a .txt file of \n delineated websites.)
+
+for example `./create_check.sh --name "Nike Home Page Test" --site https://www.nike.com`
+
---
+++ create_check.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+
+while getopts n:s: flag
+do
+    case "${flag}" in
+        n) name_of_check=${OPTARG};;
+        s) site_address=${OPTARG};;
+    esac
+done
+echo "Name: $name_of_check";
+echo "URL: $site_address";
+
+rvmcurl() {
+  local url
+  url="https://monitoring-api.rigor.com/v2/${1#/}"
+  shift || return # function should fail if we weren't passed at least one argument
+  curl -XPOST "${rvm_curl_args[@]}" "$url" "$@"
+}
+
+rvm_curl_args=(
+  -H 'content-type:application/json' 
+  -H 'API-KEY:AKTprlwpWANb6ETjnkVL' 
+)
+
+#see locations.txt file for enums for the different real browser locations
+data=$(jq -n --arg name_of_check "$name_of_check" --arg site_address "$site_address" '
+{
+    "name": $name_of_check,
+    "tags": [
+        "aantonio"
+    ],
+    
+    "frequency": 5,
+    "round_robin": true,
+    "auto_retry": false,
+    "enabled": true,
+    "locations": [
+        5,6,21,38    
+    ],
+    
+    "response_time_monitor_milliseconds": "4000",
+    "http_request_headers": {
+        "Content-Type": "text/html"
+    },
+    
+    "notifications": {
+        "sms": false,
+        "email": false,
+        "call": false,
+
+        "notify_after_failure_count": 1,
+        "notify_on_location_failure": false,
+
+        "user_agent": "Mozilla/5.0 (X11; Linux x86_64; Rigor) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36",
+        "auto_update_user_agent": false,
+        "viewport": {
+        "height": 768,
+        "width": 1366
+        },
+        "muted": true
+    },
+
+    "browser": {
+        "type": "chrome"
+    },
+  
+    "enforce_ssl_validation": true,
+    "url": $site_address,
+
+    "threshold_monitors": [
+        {
+        "matcher": "rigor.com/example",
+        "metric_name": "dom_load_time",
+        "comparison_type": "less_than",
+        "value": 3000
+        }
+    ],
+  
+    "connection": {
+        "upload_bandwidth": 5000,
+        "download_bandwidth": 20000,
+        "latency": 28,
+        "packet_loss": 0
+    },
+
+    "wait_for_full_metrics": true
+}')
+
+rvmcurl checks/real_browsers/ -d "$data"
